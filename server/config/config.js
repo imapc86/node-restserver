471753bda3bb236630fb3af0c508a6b4cc3c8f8b
Israel <ima_pc@hotmail.com>
2018-11-08 18:38:27 -0600

    Fin seccion 10, login normal

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,23 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 /*
 * =============================
+* VENCIMIENTO DEL TOKEN
+* =============================
+*/
+// 60 segundos * 60 minutos * 24 horas * 30 días
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+/*
+* =============================
+* SEED DE AUTENTICACIÓN
+* =============================
+*/
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
+
+/*
+* =============================
 * BASE DE DATOS 
 * =============================
 */
@@ -33,6 +50,3 @@ if ( process.env.NODE_ENV === 'dev' ) {
 }
 
 process.env.URLDB = urlDB;
-
-
-
